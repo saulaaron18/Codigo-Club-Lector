a923cf0e120a3fe722ed375c399b7920948c0028
Retr0TrickSt3R <iulianpricope37@gmail.com>
2026-04-11 16:55:15 +0200

    Add files via upload

--- Codigo Club Lector/src/ee1/Libro.java
+++ Codigo Club Lector/src/ee1/Libro.java
@@ -12,6 +12,50 @@ import fecha.Fecha;
 
 
 public class Libro {
+	private String titulo;
+	private String autor;
+	private Fecha fechaPublicacion;
+	private int nVecesPrestado;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	
+	public String getAutor() {
+		return autor;
+	}
+	
+	public Fecha getFechaPublicacion() {
+		return fechaPublicacion;
+	}
+	
+	public int getNVecesPrestado() {
+		return nVecesPrestado;
+	}
+	
+	public Libro(String titulo, String autor, Fecha fechaPublicacion, int nVecesPrestado) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.fechaPublicacion = fechaPublicacion;
+		this.nVecesPrestado = 0;
+	}
+	
+	public Libro(Libro obj) {
+		this.titulo = obj.getTitulo();
+		this.autor = obj.getAutor();
+		this.fechaPublicacion = new Fecha(obj.getFechaPublicacion());
+		this.nVecesPrestado = obj.getNVecesPrestado();
+	}
+	
+	public void prestado() {
+		this.nVecesPrestado += 1;
+	}
+	
+	private boolean esIgual(Libro obj) {
+		return this.titulo.compareTo(obj.getTitulo()) == 0 &&
+				this.autor.compareTo(obj.getAutor()) == 0 &&
+				this.fechaPublicacion.equals(obj.getFechaPublicacion());
+	}
 
 	/**
 	 * Indica si este libro es igual a otro
