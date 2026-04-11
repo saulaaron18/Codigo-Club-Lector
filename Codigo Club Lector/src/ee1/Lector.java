bd5119666ccaa6619819219c06d2d0248e247bef
Retr0TrickSt3R <iulianpricope37@gmail.com>
2026-04-11 18:15:57 +0200

    Add files via upload

--- Codigo Club Lector/src/ee1/Lector.java
+++ Codigo Club Lector/src/ee1/Lector.java
@@ -1,10 +1,65 @@
 package ee1;
+import list.ArrayList;
 /**
  * 
  */
 
 
 public class Lector {
+	
+	private int nSocio;
+	private String nombre;
+	private ArrayList<Libro> historicoLectura;
+	
+	public Lector(int nSocio, String nombre) {
+		this.nSocio = nSocio;
+		this.nombre = nombre;
+		historicoLectura = new ArrayList<Libro>();
+	}
+	
+	public int getNSocio() {
+		return nSocio;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public Libro getLibroLeido(int indiceHistorico) {
+		Libro leido;
+		if(indiceHistorico == 1) {
+			leido = historicoLectura.get(0);
+		} else if(indiceHistorico <= 10 && indiceHistorico > 1) {
+			leido = historicoLectura.get(indiceHistorico - 1);
+		} else {
+			leido = null;
+		}
+		return leido;
+	}
+	
+	public void leerLibro(Libro libroLeido) {
+		if(historicoLectura.size() < 10) {
+			historicoLectura.add(historicoLectura.size(), libroLeido);
+		} else if(historicoLectura.size() == 10){
+			for(int i = 1; i < historicoLectura.size(); i++) {
+				historicoLectura.set(i-1, historicoLectura.get(i));
+			}
+			historicoLectura.add(historicoLectura.size(), libroLeido);
+		}
+	}
+	
+	private boolean esIgual(Lector obj) {
+		boolean historicoIgual = false;
+		if(historicoLectura.size() == obj.historicoLectura.size()) {
+			for(int i = 0; i < historicoLectura.size(); i++) {
+				if(historicoLectura.get(i).equals(obj.historicoLectura.get(i))) {
+					historicoIgual = true;
+				}
+			}
+		}
+		return historicoIgual && nSocio == obj.getNSocio();
+	}
+	
   
   
   /**
